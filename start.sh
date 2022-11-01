8c42afffddbc754ff465e42019cbeb1da56d18ae
BenyXP <103335260+BenyXP@users.noreply.github.com>
2022-11-01 23:00:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/BenyXP/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/BenyXP/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
-pip3 install -U -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
